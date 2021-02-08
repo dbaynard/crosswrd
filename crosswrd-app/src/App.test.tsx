ba9ceab8517b9a2278309f85d7a6f8f50d307f35
David Baynard <david@baynard.dev>
2021-02-08 01:56:22 +0000

    Invert default test suite entry

--- crosswrd-app/src/App.test.tsx
+++ crosswrd-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("not default", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.queryByText(/learn react/i);
+  expect(linkElement).not.toBeInTheDocument();
 });
