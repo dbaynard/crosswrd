466f0880405bdba1100635d0baa119e75c467424
David Baynard <david@baynard.dev>
2021-02-10 21:35:52 +0000

    Create tabbed App.tsx with app name

--- crosswrd-app/src/App.tsx
+++ crosswrd-app/src/App.tsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { startCase } from "lodash";
+import { Button, Tabs, Tab } from "react-bootstrap";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import "./App.css";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+type HomeProps = { name: string };
+
+const Home = ({ name }: HomeProps) => (
+  <header className="App-header">
+    <h1>{startCase(name)}</h1>
+    <Button variant="secondary" href={`https://github.com/dbaynard/${name}`}>
+      <FontAwesomeIcon icon={faGithub} />
+    </Button>
+  </header>
 );
+
+const Tabbed = ({ name }: HomeProps) => {
+  const history = useHistory();
+  const [key, setKey] = useState<string>(history.location.pathname);
+
+  useEffect(() => {
+    if (history.location.pathname !== key) history.push(key);
+  }, [history, key]);
+
+  useEffect(() => history.listen((location) => setKey(location.pathname)), [
+    history,
+  ]);
+
+  return (
+    <Tabs activeKey={key} onSelect={(k) => setKey(k ?? "/")}>
+      <Tab eventKey="/" title="Home">
+        <Home {...{ name }} />
+      </Tab>
+    </Tabs>
+  );
+};
+
+const App = () => {
+  const name = "crosswrd";
+  return (
+    <div className="App">
+      <Router>
+        <Tabbed {...{ name }} />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
