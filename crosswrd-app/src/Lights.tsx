334359c53d8f2158883580f2e107d4eb7a917173
David Baynard <git@baynard.dev>
2021-02-12 23:44:53 +0000

    Stub lights editor

---
+++ crosswrd-app/src/Lights.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Lights = () => (
+  <Container>
+    <Row>
+      <Col>
+        <header>
+          <h1>Edit lights</h1>
+        </header>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export { Lights };
