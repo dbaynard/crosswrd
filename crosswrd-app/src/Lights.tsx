4f7545a58aa91344651f011b1f6fc25f6f8f21d8
David Baynard <david@baynard.dev>
2021-02-13 10:25:19 +0000

    Display grid in Lights component

--- crosswrd-app/src/Lights.tsx
+++ crosswrd-app/src/Lights.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 
-import { WrappedRow } from "./Helpers";
+import { CellMap, Grid } from "./Grid";
+import { displayGrid, renderCells } from "./GridDisplay";
+import { WrappedRow, StateSetter } from "./Helpers";
 
-const LightsLayout = () => (
+type LightsLayoutProps = {
+  size: bigint;
+  grid: CellMap | null;
+  setGrid: StateSetter<CellMap | null>;
+};
+const LightsLayout = ({ size, grid, setGrid }: LightsLayoutProps) => (
   <Container>
     <WrappedRow>
       <header>
         <h1>Edit lights</h1>
       </header>
     </WrappedRow>
+    <WrappedRow>
+      <Grid {...{ size }}>{renderCells(setGrid, displayGrid(size, grid))}</Grid>
+    </WrappedRow>
   </Container>
 );
 
 const Lights = () => {
-  return <LightsLayout />;
+  const size = 15n;
+  const [grid, setGrid] = useState<CellMap | null>(null);
+
+  return <LightsLayout {...{ size, grid, setGrid }} />;
 };
 
 export { Lights };
