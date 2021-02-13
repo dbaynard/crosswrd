176908ebbfa349f88f1e500c96bd23277cbf476a
David Baynard <david@baynard.dev>
2021-02-13 09:27:18 +0000

    Represent rotational symmetry of cells
    The grid is centered on (0,0), so flipping both signs is a 180°
    rotation.

--- crosswrd-app/src/Reference.tsx
+++ crosswrd-app/src/Reference.tsx
@@ -1,4 +1,4 @@
-import { Record } from "immutable";
+import { Record, Set } from "immutable";
 
 export const Reference = Record<{ x: bigint; y: bigint }>(
   { x: 0n, y: 0n },
@@ -6,3 +6,9 @@ export const Reference = Record<{ x: bigint; y: bigint }>(
 );
 const centre = Reference();
 export type Reference = typeof centre;
+
+export const rotate180 = (r: Reference): Reference =>
+  Reference({ x: -r.x, y: -r.y });
+
+export const matchingRefs = (r: Reference): Set<Reference> =>
+  Set([r, rotate180(r)]);
