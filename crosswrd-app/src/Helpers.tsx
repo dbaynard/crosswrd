9a68b6866450442d38e8a2dc272d06beea245582
David Baynard <git@baynard.dev>
2021-02-13 10:10:01 +0000

    Add StateSetter alias

--- crosswrd-app/src/Helpers.tsx
+++ crosswrd-app/src/Helpers.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 
+export type StateSetter<T> = React.Dispatch<React.SetStateAction<T>>;
+
 type WrappedRowProps = { children: JSX.Element };
 export const WrappedRow = (props: WrappedRowProps) => (
   <Row className="justify-content-center">
