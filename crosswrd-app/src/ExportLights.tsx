f5147ad62419d84917bdb28615c546d199d90734
David Baynard <david@baynard.dev>
2021-02-13 11:05:17 +0000

    Add reset button for lights

--- crosswrd-app/src/ExportLights.tsx
+++ crosswrd-app/src/ExportLights.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Button, FormControl, InputGroup } from "react-bootstrap";
 
 import { CellMap } from "./Grid";
+import { StateSetter } from "./Helpers";
 import { serializeGridLights } from "./Spiral";
 
 type ExportLightsProps = {
   grid: CellMap | null;
+  setGrid: StateSetter<CellMap | null>;
   size: bigint;
 };
 
-const ExportInput = ({ grid, size }: ExportLightsProps) => (
+const ExportInput = ({ grid, setGrid, size }: ExportLightsProps) => (
   <FormControl
     type="string"
     value={grid ? serializeGridLights(size)(grid) : ""}
@@ -20,11 +22,16 @@ const ExportInput = ({ grid, size }: ExportLightsProps) => (
   />
 );
 
-export const ExportLights = (props: ExportLightsProps) => (
+export const ExportLights = ({ setGrid, ...rest }: ExportLightsProps) => (
   <InputGroup style={{ width: "fit-content", margin: "auto" }}>
     <InputGroup.Prepend>
       <InputGroup.Text>Import/Export</InputGroup.Text>
     </InputGroup.Prepend>
-    <ExportInput {...props} />
+    <ExportInput {...{ setGrid, ...rest }} />
+    <InputGroup.Append>
+      <Button variant="outline-primary" onClick={() => setGrid(null)}>
+        Reset
+      </Button>
+    </InputGroup.Append>
   </InputGroup>
 );
