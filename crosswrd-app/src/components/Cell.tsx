f98c2da7457986a6cf637d7420ef730a7a2eb74e
David Baynard <git@baynard.dev>
2021-02-22 07:36:43 +0000

    Display letter

--- crosswrd-app/src/components/Cell.tsx
+++ crosswrd-app/src/components/Cell.tsx
@@ -1,3 +1,4 @@
+import { pick } from "lodash";
 import styled from "styled-components";
 
 import { Reference } from "../common/Reference";
@@ -18,7 +19,7 @@ const RawCell = styled.div<RawCellProps>`
   height: 100%;
   min-width: 100%;
   min-height: 100%;
-  overflow: auto;
+  overflow: hidden;
   background-color: ${(props) => (props.light ? "white" : "black")};
   place-self: center center;
   &:hover {
@@ -34,9 +35,16 @@ const RawCell = styled.div<RawCellProps>`
     top: -1px;
     padding-left: 2px;
     font-size: calc(0.2rem + 1vmin);
+    line-height: normal;
   }
+  text-transform: uppercase;
+  font-size: calc(3px + 2.5vmin);
+  text-align: center;
+  line-height: 160%;
 `;
 
 export const Cell = (props: CellProps) => (
-  <RawCell id={`${props.r.x},${props.r.y}`} {...props} />
+  <RawCell id={`${props.r.x},${props.r.y}`} {...props}>
+    {props.letter ?? ""}
+  </RawCell>
 );
